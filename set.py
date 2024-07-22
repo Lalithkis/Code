9182204515c792125580ab07be37fca9e19b76c8
Lalith Kishore V S <93479736+Lalithkis@users.noreply.github.com>
2024-07-22 16:35:04 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+n = int(input("enter the number"))  
+s = set(map(int, input().split()))  
+num_commands = int(input())
+for _in range(num_commands):
+    command = input().split()
+    if command[0] == "pop":
+        s.pop()
+    elif command[0] == "remove":
+        s.remove(int(command[1]))
+    elif command[0] == "discard":
+        s.discard(int(command[1]))
+print(sum(s))
